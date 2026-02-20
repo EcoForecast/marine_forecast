d8065cbf1ee78d0c57fd233849f1728c630f79ae
yanggu <yanggu@scc-a14.scc.bu.edu>
2026-02-20 18:22:50 -0500

    source those functions

--- 04_RUNNER.R
+++ 04_RUNNER.R
@@ -1,4 +1,7 @@
 library(dplyr)
+source("01_download_data.R")
+source("02_process_data.R")
+source("03_make_visuals.R")
 targets_raw <- download_targets()
 targets_processed <- process_targets(targets_raw)
 make_example_visualizations(
